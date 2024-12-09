244b92e432ff7cf710a18dc0146f94405fe061eb
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-09 10:49:42 +0530

    completed the navigation bar design

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -17,7 +17,16 @@ const Navigation = () => {
                     </div>
 
                     <div className="part-2">
-                        
+                        <ul className='navbar-list'>
+                            <li className="nav home">HOME</li>
+                            <li className="nav meats">MEAT</li>
+                            <li className="nav makery">BAKERY</li>
+                            <li className="nav beverage">DRINKS</li>
+                            <li className="nav blog">GROCERY</li>
+                            <li className="nav fruits">FRUITS</li>
+                            <li className="nav contact">CONTACT</li>
+                            <li className="nav about">ABOUT</li>
+                        </ul>
                     </div>
                 </div>
 
