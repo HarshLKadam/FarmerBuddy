f259235c6d1c3324effa85b0cbfb6321d13d6ca9
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-11 19:45:42 +0530

    used the city api for city names in india and log the data through axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,59 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios'
+
+const  MyContext=createContext();
 
 function App() {
+
+  const [countryList,setCountryList]=useState([])
+
+
+  const getCountry = (url) => {
+    const apiKey = import.meta.env.VITE_CITY_API_KEY;
+    
+    axios.get(url, {
+      headers: {
+        'X-CSCAPI-KEY': apiKey
+      }
+    }).then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+
+  useEffect(() => {
+    const url=import.meta.env.VITE_CITY_API_URL
+    getCountry(url);
+  }, []);
+
+
+  const values={
+
+  }
+
   return (
     <>
       <BrowserRouter>
+      <MyContext.Provider value={values}>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        </MyContext.Provider>
       </BrowserRouter>
     </>
   );
 }
 
 export default App;
+
+export {MyContext}
 
