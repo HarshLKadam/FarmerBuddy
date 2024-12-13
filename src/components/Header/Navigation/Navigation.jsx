929de447fb2015b7fc6fef7b0f47c62d029c0b85
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-13 11:06:57 +0530

    categories button was done and added the differnet categories inside it

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Navigation/Navigationcss.css'
 import { TbMenu2 } from "react-icons/tb";
 import { FaChevronDown } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
+
+     const [isOpen,setIsOpen]=useState(false)
+
     return (
         <>
             <nav>
                 <div className="navigation-bar">
                     <div className="part-1">
-                        <button className="categories">
-                            <span className='menu-icon'>  <TbMenu2 /></span>
-                            <span >All Categories</span>
-                            <span className='arrow-icon'><FaChevronDown /></span>
-                        </button>
+                        <div className="cate-wrapper">
+                            <button className="categories" onClick={()=>setIsOpen(true)}>
+                                <span className='menu-icon'>  <TbMenu2 /></span>
+                                <span >All Categories</span>
+                                <span className='arrow-icon'><FaChevronDown /></span>
+                            </button>
+                            <div className={`all ${isOpen ? 'block' : ''}`}>
+                            <button className='close' onClick={()=>setIsOpen(false)}><IoCloseSharp style={{ fontSize: '30px' }} /></button>
+                              <ul>
+                                <li><Link to='/'><button className='list-btn first'>Fruits</button></Link></li>
+                                <li><Link to='/'><button className='list-btn'>Vegitables</button></Link></li>
+                                <li><Link to='/'><button className='list-btn'>Meat and Eggs</button></Link></li>
+                                <li><Link to='/'><button className='list-btn'>Bakery</button></Link></li>
+                                <li><Link to='/'><button className='list-btn'>Grocery</button></Link></li>
+                                <li><Link to='/'><button className='list-btn'>Drinks</button></Link></li>
+                              </ul>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="part-2">
@@ -35,27 +52,27 @@ const Navigation = () => {
                                 </Link>
                             </li>
                             <li className="nav drink">
-                            <Link to='/'>
+                                <Link to='/'>
                                     <button> DRINKS</button>
                                 </Link>
                             </li>
                             <li className="nav grocery">
-                            <Link to='/'>
+                                <Link to='/'>
                                     <button>  GROCERY</button>
                                 </Link>
                             </li>
                             <li className="nav fruits">
-                            <Link to='/'>
+                                <Link to='/'>
                                     <button> FRUITS</button>
                                 </Link>
                             </li>
                             <li className="nav contact">
-                            <Link to='/'>
+                                <Link to='/'>
                                     <button> CONTACT</button>
                                 </Link>
                             </li>
                             <li className="nav contact">
-                            <Link to='/'>
+                                <Link to='/'>
                                     <button> CONTACT</button>
                                 </Link>
                             </li>
