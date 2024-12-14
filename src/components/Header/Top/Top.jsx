fdb699f5d9983597aa41c45a1bcde6fb03535ee1
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-14 10:51:14 +0530

    updated some header part and css and added location dropdown file into main Header file

---
+++ src/components/Header/Top/Top.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Top/Topcss.css'
+
+const Top = () => {
+    return (
+        <div className="top">
+                <p className="welcome ">
+                    "Welcome to FarmerBuddy – Empowering Farmers, Connecting Communities!"
+                </p>
+        </div>
+    )
+}
+
+export default Top
