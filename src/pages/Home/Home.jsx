4a8c5145e669e5e7fce1b97adf40a8a225e57f92
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-16 07:35:39 +0530

    creating the product section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,55 @@ import HomeBanner from '../Home/HomeBanner/HomeBanner.jsx'
 import Ad1 from '../../assets/ad1.jpg'
 import Ad2 from '../../assets/ad2.jpg'
 import '../Home/HomeCss.css'
+import Slider from "react-slick";
+
 
 const Home = () => {
+  var settings = {
+  
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
   return (
-   <>
-   <HomeBanner/>
-<div className="line"></div>
-   <div className="homeProducts">
-     <div className="left ad-section">
-        <img src={Ad1} alt="" />
-        <img src={Ad2} alt="" />
-     </div>
+    <>
+      <HomeBanner />
 
-     <div className="right">
+      <div className="homeProducts">
+        <div className="ad-container">
+          <div className="left">
+            <img src={Ad1} alt="" />
+            <img src={Ad2} alt="" />
+          </div>
+        </div>
 
-     </div>
-   </div>
-   </>
+        <div className="right">
+
+          <div className="head">
+            <div className="title">
+              <span className='cap'>BEST SELLERS </span><br />
+              <span className='bottom'> Do not miss this the current offers until the end of this Month</span>
+            </div>
+            <div className="view-all">
+              <button> View All</button>
+            </div>
+          </div>
+
+          <div className="product">
+            <Slider {...settings}>
+              <div className=" product-items item">
+                 
+              </div>
+            </Slider>
+          </div>
+
+        </div>
+
+      </div>
+    </>
   )
 }
 
