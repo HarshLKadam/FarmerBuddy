42f6a0a55a6d76fc06abc0f2cfa7a28ef429215b
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-19 17:57:11 +0530

    creating home product

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,15 @@ import HomeBanner from '../Home/HomeBanner/HomeBanner.jsx'
 import Ad1 from '../../assets/ad1.jpg'
 import Ad2 from '../../assets/ad2.jpg'
 import '../Home/HomeCss.css'
+import product1 from '../../assets/p1.jpeg'
+import product2 from '../../assets/product2.jpg'
 import Slider from "react-slick";
 
 
 const Home = () => {
-  var settings = {
-  
-    infinite: true,
+  var productSlider = {
+
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -20,37 +22,75 @@ const Home = () => {
     <>
       <HomeBanner />
 
-      <div className="homeProducts">
-        <div className="ad-container">
-          <div className="left">
-            <img src={Ad1} alt="" />
-            <img src={Ad2} alt="" />
-          </div>
+      <div className="home">
+
+        <div className="left">
+          <img src={Ad1} alt="" />
+          <img src={Ad2} alt="" />
         </div>
 
         <div className="right">
 
-          <div className="head">
-            <div className="title">
-              <span className='cap'>BEST SELLERS </span><br />
-              <span className='bottom'> Do not miss this the current offers until the end of this Month</span>
-            </div>
-            <div className="view-all">
+          <div className='info'>
+            <div className="head">
+              <span>BEST SELLERS</span>
               <button> View All</button>
             </div>
+            <div className="slogan">
+              Do not miss this offer
+            </div>
           </div>
+
 
           <div className="product">
-            <Slider {...settings}>
-              <div className=" product-items item">
-                 
-              </div>
-            </Slider>
-          </div>
 
+            <div className="items">
+              <img src={product1} alt="" />
+              <div className="desc">
+                <span>Tommato</span>
+                <span>20Rs/KG</span>
+              </div>
+             
+            </div>
+
+            <div className="items">
+              <img src={product2} alt="" />
+              <div className="desc">
+                <span>Onion</span>
+                <span>40Rs/KG</span>
+              </div>
+           
+            </div>
+            
+            <div className="items">
+              <img src={product1} alt="" />
+              <div className="desc">
+                <span>Tommato</span>
+                <span>20Rs/KG</span>
+              </div>
+            </div>
+
+            <div className="items">
+              <img src={product1} alt="" />
+              <div className="desc">
+                <span>Tommato</span>
+                <span>20Rs/KG</span>
+              </div>
+            </div>
+
+            <div className="items">
+              <img src={product1} alt="" />
+              <div className="desc">
+                <span>Tommato</span>
+                <span>20Rs/KG</span>
+              </div>
+            </div>
+
+          </div>
         </div>
 
       </div>
+
     </>
   )
 }
