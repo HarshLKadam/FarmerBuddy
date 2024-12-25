8fb5a41314355a0e16dde28d20dcb5a8ddc26aa9
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-25 17:17:58 +0530

    added list and grid view to products

---
+++ src/pages/Home/ProductItem/ListItem.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import pd3 from '../../../assets/pd3.webp'
+import { Link } from 'react-router-dom'
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+
+const ListItem = () => {
+  return (
+    <div>
+       <div className="item w-[200px] h-[340px] border p-4 shadow-md cursor-pointer transition-all ease-in-out hover:scale-105">
+    <div className="image overflow-hidden object-cover mb-4">
+        <img src={pd3} alt="" />
+    </div>
+    <div className="info text-center">
+        <h6>
+            <Link to='/' className='no-underline text-black transition-all'>
+                Fresh Fruits
+            </Link>
+        </h6>
+        <h3>
+            <Link to='/' className='no-underline text-black transition-all hover:text-green-500'>
+                Apple
+            </Link>
+        </h3>
+        <div className="rating ml-3 mt-2">
+            <Stack spacing={1}
+                className=''>
+                <Rating name="size-medium" defaultValue={4} readOnly />
+            </Stack>
+        </div>
+        <div className="price mt-2">
+            <h4>
+                <Link to='/' className='no-underline text-black transition-all hover:text-green-500'>
+                    40Rs/kg
+                </Link>
+            </h4>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default ListItem
--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import SideBar from './SideBar/SideBar';
 import Item from '../Home/ProductItem/Item';
+import ListItem from '../Home/ProductItem/ListItem';
+
 import { Button } from '@mui/material';
 import { IoGrid } from "react-icons/io5";
 import { CiGrid2H } from "react-icons/ci";
@@ -20,37 +22,33 @@ const ProductListing = () => {
     setAnchorEl(null);
   };
 
-  const [itemView, setItemView] = useState('grid')
+  const [itemView, setItemView] = useState('grid');
 
   return (
     <div className="py-8">
       <div className="container flex gap-3">
-
         <div className="sidebar w-[20%] h-full bg-green-200 p-3">
           <SideBar />
         </div>
 
-
         <div className="right w-[80%]">
-
           <div className="bg-green-300 w-full mb-3 rounded-md flex items-center justify-between p-3">
             <div className="flex items-center gap-3">
-              <Button className="w-[30px] h-[40px] min-w-[40px] rounded-full"
-                onClick={() => {
-                  setItemView('grid')
-                }}>
+              <Button
+                className='w-[40px] h-[40px] min-w-[40px] rounded-full'
+                onClick={() => setItemView('grid')}
+              >
                 <IoGrid style={{ fontSize: '20px', color: 'black' }} />
               </Button>
 
-              <Button className="w-[30px] h-[40px] min-w-[40px] ml-[20px] rounded-full"
-                onClick={() => {
-                  setItemView('grid')
-                }}>
-                <CiGrid2H style={{ fontSize: '20px', color: 'black' }} />
+              <Button
+                className='w-[40px] h-[40px] min-w-[40px] ml-[20px] rounded-full'
+                onClick={() => setItemView('list')}
+              >
+                <CiGrid2H style={{ fontSize: '20px', color:'black' }} />
               </Button>
 
               <span className="font-bold">There are 30 Products found</span>
-
             </div>
 
             <div className="sort">
@@ -60,7 +58,7 @@ const ProductListing = () => {
                 aria-haspopup="true"
                 aria-expanded={open ? 'true' : undefined}
                 onClick={handleClick}
-                className='text-black border border-black rounded-full'
+                className="text-black border border-black rounded-full"
               >
                 Sort By
               </Button>
@@ -73,31 +71,36 @@ const ProductListing = () => {
                   'aria-labelledby': 'basic-button',
                 }}
               >
-                <MenuItem onClick={handleClose}>Price Low to high</MenuItem>
-                <MenuItem onClick={handleClose}>Price High to Low </MenuItem>
+                <MenuItem onClick={handleClose}>Price Low to High</MenuItem>
+                <MenuItem onClick={handleClose}>Price High to Low</MenuItem>
                 <MenuItem onClick={handleClose}>Name A to Z</MenuItem>
                 <MenuItem onClick={handleClose}>Name Z to A</MenuItem>
               </Menu>
             </div>
           </div>
 
-
-          <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
-            {
-              itemView === 'grid' ?
-                <>
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                </> 
-                :
-          }
-
+          <div className={itemView === 'grid' ? 'grid grid-cols-2 gap-3 md:grid-cols-4' : 'flex flex-col gap-3'}>
+            {itemView === 'grid' ? (
+              <>
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+              </>
+            ) : (
+              <>
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+              </>
+            )}
           </div>
         </div>
       </div>
