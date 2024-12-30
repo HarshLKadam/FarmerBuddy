d1ec586886ffb9cbb64d5deaf8a2a656112ab7b8
Harsh Kadam <harshkadam7474@gmail.com>
2024-12-30 17:59:17 +0530

    added forgot passsword page

--- src/pages/Login/ForgotPassword/ForgotPassword.jsx
+++ src/pages/Login/ForgotPassword/ForgotPassword.jsx
@@ -13,14 +13,15 @@ import Button from '@mui/material/Button';
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
-import { MyContext } from '../../App';
+import { MyContext } from '../../../App.jsx'
 
-
-const Login = () => {
-
+const ForgotPassword = () => {
     const context = useContext(MyContext);
 
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);  
+
+    const [showPassword2, setShowPassword2] = useState(false);  
+
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -33,6 +34,17 @@ const Login = () => {
     };
 
 
+    
+    const handleClickShowPassword2 = () => setShowPassword2((show) => !show);
+
+    const handleMouseDownPassword2 = (event) => {
+        event.preventDefault();
+    };
+
+    const handleMouseUpPassword2 = (event) => {
+        event.preventDefault();
+    };
+
     const [formFields, setFormFields] = useState({
         email: '',
         password: ''
@@ -40,21 +52,14 @@ const Login = () => {
 
     const navigate = useNavigate()
 
-    //for password forgot 
-    const forgotPassword = () => {
-        context.opentoast('success','OTP sended successfully')
-        // context.opentoast('error','error')
-        navigate('/verify')
-
-    }
 
     return (
         <div className='py-10 '>
             <div className="container">
-                <div className="card shadow-md w-[500px] m-auto bg-white p-4">
+                <div className="card shadow-md w-[400px] m-auto bg-white p-4">
 
                     <h4 className='text-center font-bold'>
-                        Login to your Account
+                        Forgot Password
                     </h4>
 
                     <div className='w-full mt-4'>
@@ -63,27 +68,22 @@ const Login = () => {
 
                             <Box
                                 component="form"
-                                sx={{ '& > :not(style)': { m: 1, width: '45ch' } }}
+                                sx={{ '& > :not(style)': { m: 1, width: '35ch' } }}
                                 noValidate
                                 autoComplete="off"
                                 className='text-black' >
 
-                                <TextField
-                                    id="standard-basic"
-                                    label="UserName / Email Id"
-                                    variant="outlined"
-                                    className='w-full text-black mt-2'
-                                    name='email'
-                                />
+
 
                                 <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
 
                                     <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
 
                                     <OutlinedInput
-                                        id="outlined-adornment-password"
+                                        id="password"
                                         type={showPassword ? 'text' : 'password'}
                                         name='password'
+                                        label='password'
                                         endAdornment={
 
                                             <InputAdornment position="end">
@@ -100,48 +100,50 @@ const Login = () => {
                                                 </IconButton>
                                             </InputAdornment>
                                         }
-
-                                        label="Password"
                                     />
+
+                                </FormControl>
+
+                                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+
+                                    <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+
+                                    <OutlinedInput
+                                        id="confirm-password"
+                                        type={showPassword2 ? 'text' : 'password'}
+                                        name='confirm-password'
+                                        label='confirm-password'
+                                        endAdornment={
+
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label={
+                                                        showPassword2 ? 'hide the password' : 'display the password'
+                                                    }
+                                                    onClick={handleClickShowPassword2}
+                                                    onMouseDown={handleMouseDownPassword2}
+                                                    onMouseUp={handleMouseUpPassword2}
+                                                    edge="end"
+                                                >
+                                                    {showPassword2 ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+
                                 </FormControl>
 
                             </Box>
 
-
-                            <p className='ml-3 mt-4 hover:text-green-400 cursor-pointer'
-                                onClick={forgotPassword()}> Forgat Password ?
-                            </p>
-
-
-                            <div className="button flex justify-center">
+                            <div className="button flex justify-center mt-4">
                                 <Button
                                     className='text-black border border-gray-500  px-4 bg-green-400 w-full'
-                                    style={{ backgroundColor: 'rgb(59 255 59)' }}>LogIn
+                                    style={{ backgroundColor: 'rgb(59 255 59)' }}>Change Password
                                 </Button>
                             </div>
 
-                            <p className='mt-2 ml-3 text-gray-400 '>
-                                Not Registered?
-                                <Link
-                                    to='/register'
-                                    className='no-underline text-gray-600 ml-2 text-[16px] font-bold hover:text-green-500'>
-                                    Sign Up
-                                </Link>
-                            </p>
 
-                            <p className='text-center text-gray-400 '>
-                                Or continue with Social Account
-                            </p>
 
-                            <div className="google-button text-center">
-
-                                <Button
-                                    className='text-black border border-black flex gap-4 px-4 w-full'>
-                                    <FcGoogle style={{ fontSize: '30px' }} />
-                                    <span>Sign in With Google</span>
-                                </Button>
-
-                            </div>
                         </div>
 
                     </div>
@@ -152,4 +154,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default ForgotPassword
